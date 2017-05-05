c797a6eda4ee373c3391dde55c2f1f9424a768e6
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-05-05 09:08:28 +0300

    Регистрация Css файла в виде

--- views/post/show.php
+++ views/post/show.php
@@ -1,5 +1,6 @@
 <h1>Show action</h1>
 
 <?php //$this->registerJsFile('@web/js/scripts.js', ['depends' => 'yii\web\YiiAsset']); ?>
-<?php $this->registerJs('var message = \'Wuff\';
-    $(\'.container\').append(message);'); ?>
+<?php //$this->registerJs('var message = \'Wuff\';
+    //$(\'.container\').append(message);', \yii\web\View::POS_LOAD); ?>
+<?php $this->registerCss('.container {background: #ccc}');?>
