dfdc40aee9bd5bc68005cba7bad81001d22e450f
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-05-11 07:40:39 +0300

    Ограничиваем выборку limit

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -42,10 +42,13 @@ class PostController extends AppController
         $this->view->registerMetaTag(['name' => 'keywords', 'content' => 'Ключевые слова']);
         $this->view->registerMetaTag(['name' => 'description', 'content' => 'Описание страницы']);
 
+        //$cats = Category::find()->orderBy(['id' => SORT_DESC])->all();
+        //$cats = Category::find()->all();
         //$cats = Category::find()->asArray()->where('parent = 691')->all();
         //$cats = Category::find()->asArray()->where(['parent' => 691])->all();
         //$cats = Category::find()->asArray()->where(['like', 'title', 'pp'])->all();
-        $cats = Category::find()->asArray()->where(['<=', 'id', 695])->all();
+        //$cats = Category::find()->asArray()->where(['<=', 'id', 695])->all();
+        $cats = Category::find()->asArray()->where(['parent' => 691])->limit(1)->all();
 
 
         return $this->render('show', compact('cats'));
